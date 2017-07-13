fcaab92fd1f18a5673d20c406c6b5207df672aa0
Bradley Vorjohan <bhvorjo@gmail.com>
2017-07-13 00:47:10 -0500

    Wrote new vectorizer module. Now a pretty successful algorithm.

--- program_runner.py
+++ program_runner.py
@@ -2,6 +2,7 @@ import tf_idf
 import Scraper
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
 '''
 reference:
@@ -20,7 +21,8 @@ http://blog.christianperone.com/2011/09/machine-learning-text-feature-extraction
 
 
 beginning_url = "https://en.wikipedia.org/wiki/Trigonometry"
-end_url = "https://en.wikipedia.org/wiki/Abraham_Lincoln"
+beginning_url = "https://en.wikipedia.org/wiki/Special:Random"
+end_url = "https://en.wikipedia.org/wiki/Jesus"
 
 def Wikipediaize(url):
     try:
@@ -36,16 +38,20 @@ seed_text = Scraper.get_text(end_url)
 # debug first text
 
 # vectorize target, use in predictor
-vectorizer = TfidfVectorizer()
-# print(seed_text)
-vectorizer.fit([seed_text])
-print()
-print(vectorizer.idf_[vectorizer.vocabulary_["how"]])
-matr = vectorizer.transform([Scraper.get_text(beginning_url)])
+# vectorizer = TfidfVectorizer()
+vectorizer = tf_idf.Vorjo_Vectorizer(common_freq = 1, rare_freq = 0)
 
-word_in_question = "most"
-print(word_in_question,matr[(0,vectorizer.vocabulary_[word_in_question])])
-end_matrix = vectorizer.transform([seed_text])
+
+vectorizer.fit(seed_text)
+# vectorizer.print_letter_frequency_inv_order()
+
+# exit()
+matr = vectorizer.transform(Scraper.get_text(beginning_url))
+
+# word_in_question = "he"
+# print(word_in_question,matr[vectorizer.cut_vocab[word_in_question]])
+end_matrix = vectorizer.transform(seed_text)
+# exit()
 # print(matr)
 # print(matr2)
 # print(np.dot(matr,matr2.T).todense())
@@ -57,13 +63,14 @@ active_url = beginning_url
 winner = ""
 steps = [beginning_url]
 print("Runnin....")
+text_bank = {}
 for _ in range(20):
     urls = Scraper.get_links(active_url)
     tf_idf_vectors = {}
     most_similar_url = ""
     biggest_dot = 0
     count = 0
-    if "/wiki/Abraham_Lincoln" in urls:
+    if "/wiki/Jesus" in urls:
         print("U WIN OMGGGGGG")
         winner = "u my frand"
         break
@@ -75,27 +82,39 @@ for _ in range(20):
             print("Parsing URL...")
             # print(url)
             print(Wikipediaize(url))
-            print(end_url)
+            # print(end_url)
         if Wikipediaize(url) == end_url:
             print("U WIN OMG")
             break
-        if url not in tf_idf_vectors:
+        if (url not in tf_idf_vectors) and (Wikipediaize(url) not in steps):
+            # if url in text_bank:
+                # url_vector = text_bank[url]
+
+
             try:
-                url_vector = vectorizer.transform([Scraper.get_text("https://en.wikipedia.org"+url)])
+                if url in text_bank:
+                    url_vector = text_bank[url]
+                else:
+                    url_vector = vectorizer.transform(Scraper.get_text("https://en.wikipedia.org"+url))
+                    text_bank[url] = url_vector
             except:
                 pass
             else:
-                dotted = np.dot(url_vector,end_matrix.T).todense()
+                dotted = np.dot(np.array(url_vector),np.array(end_matrix).T)
                 tf_idf_vectors[url] = dotted
                 if False:
                     print("Similarity: "+str(dotted))
                 if (dotted > biggest_dot):
                     biggest_dot = dotted
                     most_similar_url = url
+                    print("Current leader: " + url)
+        # del url_vector
 
     print("Winner: " + most_similar_url + " with value: " + str(biggest_dot))
+    print(len(text_bank))
     active_url = Wikipediaize(most_similar_url)
     steps.append(active_url)
+
 
 if winner:
     print("YAY YOU WON!")
--- tf_idf.py
+++ tf_idf.py
@@ -3,61 +3,91 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 # input("Press Enter to continue...")
 
-def evaluate_recall(y, y_test, k=1):
-    pass
-    num_examples = float(len(y))
-    num_correct = 0
-    for predictions, label in zip(y, y_test):
-        if label in predictions[:k]:
-            num_correct += 1
-    return num_correct/num_examples
+class Vorjo_Vectorizer:
 
-# Random Predictor
-def predict_random(context, utterances):
-    pass
-    return np.random.choice(len(utterances), 10, replace=False)
+    def __init__(self, common_freq = 1, rare_freq = 0):
+        self.vocabulary = {}
+        self.cut_vocab = {}
+        self.tf = []
+        self.common_freq = common_freq
+        self.rare_freq = rare_freq
 
-# Evaluate Random predictor
-'''
-y_random = [predict_random(test_df.Context[x], test_df.iloc[x,1:].values) for x in range(len(test_df))]
-y_test = np.zeros(len(y_random))
-for n in [1, 2, 5, 10]:
-    print("Recall @ ({}, 10): {:g}".format(n, evaluate_recall(y_random, y_test, n)))
-'''
+    def fit(self, doc):
+        word_list = doc.split()
+        for word_index in range(len(word_list)):
+            new_word = ""
+            for char in word_list[word_index]:
+                if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-''":
+                    new_word += char.lower()
+            if len(new_word.strip()) > 0:
+                word_list[word_index] = new_word.strip()
 
+        for word in word_list:
+            if word in self.vocabulary:
+                self.tf[self.vocabulary[word]] += 1
+            else:
+                self.vocabulary[word] = len(self.tf)
+                self.tf.append(1)
+        length = np.linalg.norm(self.tf)
 
+        for i in range(len(self.tf)):
+            self.tf[i] = self.tf[i]/length
 
-class TFIDFPredictor:
-    def __init__(self):
-        self.vectorizer = TfidfVectorizer()
+        self.update_cut_vocab()
 
-    def train(self, data):
-        self.vectorizer.fit(np.append(data.Context.values,data.Utterance.values))
+    def print_dict(self):
+        for word in self.vocabulary:
+            print(word)
 
-    def predict(self, context, utterances):
-        # Convert context and utterances into tfidf vector
-        vector_context = self.vectorizer.transform([context])
-        vector_doc = self.vectorizer.transform(utterances)
-        # The dot product measures the similarity of the resulting vectors
-        result = np.dot(vector_doc, vector_context.T).todense()
-        result = np.asarray(result).flatten()
-        # Sort by top results and return the indices in descending order
-        return np.argsort(result, axis=0)[::-1]
+    def print_cut_dict(self):
+        for word in self.cut_vocab:
+            print(word)
 
+    def print_letter_frequency(self):
+        for word in self.vocabulary:
+            print(word + " " + str(self.tf[self.vocabulary[word]]))
 
-# Evaluate TFIDF predictor
-'''
-pred = TFIDFPredictor()
-pred.train(train_df)
-y = [pred.predict(test_df.Context[x], test_df.iloc[x,1:].values) for x in range(len(test_df))]
-for n in [1, 2, 5, 10]:
-    print("Recall @ ({}, 10): {:g}".format(n, evaluate_recall(y, y_test, n)))
-'''
+    def print_letter_frequency_inv_order(self):
+        new_vocab = dict(self.vocabulary)
+        new_tf = self.tf[:]
 
+        while len(new_vocab) > 0:
+            min_freq = 1
+            min_word = ""
+            # print(len(new_vocab))
+            for word in new_vocab:
+                # print(word, new_tf[new_vocab[word]])
+                if new_tf[new_vocab[word]] < min_freq:
+                    min_freq = new_tf[new_vocab[word]]
+                    min_word = word
 
+            print(min_word + " " + str(min_freq))
+            # del new_tf[new_vocab[word]]
+            del new_vocab[min_word]
 
+    def update_cut_vocab(self):
+        new_vocab = {}
+        for word in self.vocabulary:
+            freq = self.tf[self.vocabulary[word]]
+            if (freq < self.common_freq) and (freq > self.rare_freq):
+                new_vocab[word] = self.vocabulary[word]
+        self.cut_vocab = new_vocab
 
-# --------------------------------
+    def transform(self, doc):
+        word_list = doc.split()
+        for word_index in range(len(word_list)):
+            new_word = ""
+            for char in word_list[word_index]:
+                if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-''":
+                    new_word += char.lower()
+            if len(new_word.strip()) > 0:
+                word_list[word_index] = new_word.strip()
 
-beginning_url = "https://en.wikipedia.org/wiki/Trigonometry"
-end_url = "https://en.wikipedia.org/wiki/Adolf_Hitler"
+        vector = [0]*len(self.tf)
+        for word in word_list:
+            if word in self.cut_vocab:
+                vector[self.cut_vocab[word]] += 1
+
+        norm = np.linalg.norm(vector)
+
+        return [i/norm for i in vector]
